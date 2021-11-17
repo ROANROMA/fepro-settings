5a35a5d7e06ebca217954799f5b62efb0c347332
Andresolg <andresolgin.rodriguez@gmail.com>
2021-11-17 10:37:27 -0500

    cifrado

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+/** @format */
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('config.json');
 const middlewares = jsonServer.defaults();
 
+// config.json usuario, password cifrados con SHA256
 server.use(middlewares);
 server.use(router);
 server.listen(process.env.PORT || 5000, () => {
